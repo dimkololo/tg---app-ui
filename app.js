65462e71d8e0a2f5e0ca498beb5632f4952aefa6
Dmitriy <58990019+dimkololo@users.noreply.github.com>
2025-10-25 14:58:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -654,8 +654,8 @@ try {
 }
 
 
-    // 1) запускаем кулдаун (10 мин обычный / 5 мин премиум)
-    const COOLDOWN_MIN = window.PLAM.premium ? 5 : 10;
+    // 1) запускаем кулдаун (30 мин обычный / 20 мин премиум)
+    const COOLDOWN_MIN = window.PLAM.premium ? 20 : 30;
     window.PLAM.cooldownUntil = Date.now() + COOLDOWN_MIN*60*1000;
 
     // 2) очищаем форму (попап НЕ закрываем)
